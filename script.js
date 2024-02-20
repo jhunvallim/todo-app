84c1d64debde95ab3908bfbedeec58e907be3b3a
Helio Vallim <h.vallim@gmail.com>
2024-02-19 21:38:32 -0300

    feat: cria os event listener para abertura e fechamento do modal

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 
 let currentTask = {};
+
+openTaskFormBtn.addEventListener("click", () => {
+  taskForm.classList.toggle("hidden");
+});
+
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+});
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
